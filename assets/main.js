4408cd71ad0d64bc9e2d0481ae077d15a74f5f52
O-pus <hongluong296@gmail.com>
2021-11-25 13:02:04 +0700

    add navigation

---
+++ assets/main.js
@@ -0,0 +1,18 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const dropdowns = $$(".dropdown");
+const subtitle = $$(".subtitle");
+
+dropdowns.forEach((dropdown, index) => {
+  dropdown.onmouseover = function () {
+    if (subtitle[index]) {
+      subtitle[index].classList.toggle("display");
+    }
+  };
+  dropdown.onmouseout = function () {
+    if (subtitle[index]) {
+      subtitle[index].classList.toggle("display");
+    }
+  };
+});
