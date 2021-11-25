165349db960ed6278b4aa230e6e73987014c290a
O-pus <hongluong296@gmail.com>
2021-11-25 23:03:31 +0700

    responsive nav

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,7 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+//Select Element
 const dropdowns = $$(".dropdown");
 const subtitle = $$(".subtitle");
 
